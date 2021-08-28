c6cdc52356e93f14154a5f7b646b33a12871e2bc
Svyatoslav2020 <89709451+Svyatoslav2020@users.noreply.github.com>
2021-08-29 01:00:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,12 +16,9 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-
-@dp.message_handler()
-def echo_message(msg: types.Message):
-    f = open(msg.text)
-    bot.send_document(msg.from_user.id, f)
-    f.close()
+f = open("~/vseapps/bot.py")
+bot.send_document(msg.from_user.id, f)
+f.close()
 
 
 if __name__ == '__main__':
